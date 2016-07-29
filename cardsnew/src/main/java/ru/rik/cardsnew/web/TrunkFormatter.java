24cd1c83b0350e8708ef06b04f8aecec0589ebd2
gus75 <gsv@10.33.91.6>
2016-07-29 11:00:00 +0300

    t

--- cardsnew/src/main/java/ru/rik/cardsnew/web/TrunkFormatter.java
+++ cardsnew/src/main/java/ru/rik/cardsnew/web/TrunkFormatter.java
@@ -9,18 +9,24 @@ import ru.rik.cardsnew.domain.Trunk;
 
 public class TrunkFormatter implements Formatter<Trunk> {
 
+	
 	public TrunkFormatter() {
+		super();
 	}
 
 	@Override
 	public String print(Trunk trunk, Locale locale) {
+		System.out.println("!!! print id: " + trunk.getId());
 		return String.valueOf(trunk.getId());
 	}
 
 	@Override
-	public Trunk parse(String id, Locale locale) throws ParseException {
+	public Trunk parse(String text, Locale locale) throws ParseException {
+		System.out.println("!!! parse");
+		long id = Long.parseLong(text);
 		Trunk t = new Trunk();
-		t.setId(Long.parseLong(id));
+		System.out.println("!!! parse id: " + id);
+
 		return t;
 	}
 
