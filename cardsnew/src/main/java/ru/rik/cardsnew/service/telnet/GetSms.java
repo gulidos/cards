6c4d7bc31808e258c5ed0abdb7977ee3f9390c06
gus75 <gsv@192.168.88.247>
2016-11-11 23:04:15 +0300

    h

--- cardsnew/src/main/java/ru/rik/cardsnew/service/telnet/GetSms.java
+++ cardsnew/src/main/java/ru/rik/cardsnew/service/telnet/GetSms.java
@@ -17,7 +17,7 @@ public class GetSms {
 			Channel ch = new Channel();
 			System.out.print("Читаем sms из " + ch.getName() + " " + " ...  ");
 			TelnetHelper t = new TelnetHelper();
-			TelnetClient tc  = t.getConnection("172.17.1.34" , 8223, "voip", "1234");
+			TelnetClient tc  = t.getConnection("172.17.1.34" , 8023, "voip", "1234");
 //			TelnetClient tc  = t.getConnection("192.168.5.206" , 8223, "voip", "1234");
 			for (int i = 1; i <= 2; i++) {
 				System.out.println("module " + i);
