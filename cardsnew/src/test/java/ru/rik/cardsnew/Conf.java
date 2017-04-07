8275888c0af529b40d9fc78bc2fb75687eecccfa
gus75 <gsv@192.168.88.249>
2017-04-07 17:20:06 +0300

    for rules

---
+++ cardsnew/src/test/java/ru/rik/cardsnew/Conf.java
@@ -0,0 +1,89 @@
+package ru.rik.cardsnew;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedBuilderParametersImpl;
+import org.apache.commons.configuration2.builder.ReloadingDetectorFactory;
+import org.apache.commons.configuration2.builder.ReloadingFileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.configuration2.io.FileHandler;
+import org.apache.commons.configuration2.reloading.ManagedReloadingDetector;
+import org.apache.commons.configuration2.reloading.PeriodicReloadingTrigger;
+import org.apache.commons.configuration2.reloading.ReloadingDetector;
+
+public class Conf {
+	ReloadingFileBasedConfigurationBuilder<FileBasedConfiguration> builder;
+	public Conf() {
+		File file = new File("~/application.properties");
+		
+		Parameters params = new Parameters();
+
+		builder =
+		    new ReloadingFileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+		    .configure(params.properties()
+		    		
+		    		.setFile(file)
+		    		.setReloadingDetectorFactory(new ReloadingDetectorFactory() {
+						public ReloadingDetector createReloadingDetector(FileHandler handler, FileBasedBuilderParametersImpl params)
+								throws ConfigurationException {
+							return new ManagedReloadingDetector();
+						}
+					})
+		     );
+		
+		 PeriodicReloadingTrigger trigger = new PeriodicReloadingTrigger(builder.getReloadingController(),  null, 1, TimeUnit.MINUTES);
+		
+	}
+	
+	
+	public Configuration reloadConfigurationFile() {
+		boolean isReloadSuccessful = builder.getReloadingController().checkForReloading(null);
+		Configuration config = null;
+		if (isReloadSuccessful) {
+			try {
+				 config = builder.getConfiguration();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				isReloadSuccessful = false;
+			}
+		}
+
+		return config;
+	}
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		Conf conf = new Conf();
+		try {
+			Configuration config = conf.builder.getConfiguration(); 
+			
+			String dbHost = config.getString("database.host");
+			int dbPort = config.getInt("database.port");
+			System.out.println(dbHost + " " + dbPort);
+			
+			Thread.sleep(5000);
+
+//			 
+//			ReloadingController controller = conf.builder.getReloadingController();
+//			ManagedReloadingDetector strategy = (ManagedReloadingDetector) controller.getDetector();
+//			strategy.refresh();
+//			
+			
+			config = conf.reloadConfigurationFile(); 
+//					conf.builder.getConfiguration(); 
+
+			System.out.println(config.getString("database.host") + " " + config.getInt("database.port"));
+			
+			
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ cardsnew/src/test/java/ru/rik/cardsnew/Rules.java
@@ -0,0 +1,86 @@
+package ru.rik.cardsnew;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Rules {
+	private List<String[]> rules;
+	
+	public Rules() {
+	}
+	
+	public void load(List<String> list) {
+		if (list == null)
+			return;
+		rules = new ArrayList<>();
+		for (String s: list) {
+			String [] a = s.split("\\s+");
+			if	(a.length == 2) {
+				a = Arrays.copyOf(a, 3);  // if the rule hasn't prefix that has to be added
+				a[2] = "";
+			}	
+				
+			if (isOk(a))
+				rules.add(a);
+		}	
+	}
+	
+	
+	private boolean isOk(String[] a) {
+		if (a.length != 3)
+			return false;
+		for (String s: a) {
+			for (char ch: s.toCharArray())
+				if (!Character.isDigit(ch)) {
+					System.err.println(s + " is not verified");
+					return false;
+				}	
+		}
+		return true;
+	}
+	
+	
+	private String[] find(String number) {
+		if (number == null || number.length() == 0)
+			return null;
+		String[] res = null;
+		int longest = 0;
+		for (String[] a: rules) {
+			if (number.startsWith(a[0]) && a[0].length() > longest) {
+				res = a;
+				longest = a[0].length();
+			}	
+		}
+		System.out.println(Arrays.toString(res));
+		return res;
+	}
+	
+	
+	public String apply(String n) {
+		if (n == null || n.length() == 0) return null;
+		String[] rule = find(n);
+		if (rule == null ) 
+			return n;	
+		
+		int cut = Integer.parseInt(rule[1]);
+		if (cut == 0 || n.length() <= cut )
+			return n;
+		
+		return (rule[2] + n.substring(cut));	
+	}
+	
+	
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList(new String[] {
+			"7		 0	   	0",
+			"7495775		 4	   	",
+			"7495 1	  8  "
+		});
+		Rules r = new Rules();
+		r.load(list);
+		
+		
+		System.out.println(r.apply("74958757575"));
+	}
+}
